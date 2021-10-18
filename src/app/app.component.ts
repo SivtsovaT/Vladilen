7ccb7e37f514482e65b779982277371849ddf236
user <sivtsovatanya1985@gmail.com>
2021-10-18 16:22:24 +0300

    Hide cards
    - new structure into app.component.html;
    - added *ngIf = toggle and button Toggle cards;
    - created ng-template #noCards;
    - added else into div with cards;
    - added export interface Card;
    - added export class toggle;
    - added Cards object;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
+  date?: any
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Vladilen';
+
+  public todos = [
+    {id:1, title: 'Купить хлеб', completed: false, date: new Date()},
+    {id:1, title: 'Купить масло', completed: true, date: new Date()},
+    {id:1, title: 'Купить пиво', completed: false, date: new Date()}
+
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card/card.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule
